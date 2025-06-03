c9c2be94e6f7bf67ed0d6c5dffd7504eeaa5abc9
parasix <parasixparasix@gmail.com>
2025-06-03 14:29:09 +0700

    Update vless.js

--- vless.js
+++ vless.js
@@ -13,9 +13,9 @@ const tags = `#Group Telegram : https://t.me/mistakkee_bot
 `;
 
 // Contoh penggunaan dalam handler utama
-export default async function handler(request: Request): Promise<Response> {
-    try {
-        const domainvless = request.headers.get("Host");
+export default async function handler(request) {
+  try {
+    const domainvless = request.headers.get("Host");
                
  if (request.url.endsWith('/favicon.ico')) {
         // Link ke gambar yang ingin dijadikan favicon
